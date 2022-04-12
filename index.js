b0c1d810155f49f61cfdfbf8085d8cdafecd8896
Tyler <tylerclancy030@gmail.com>
2022-04-12 12:49:19 -0500

    Test commit

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/test', function (req, res) {
     res.send('Fail test')
 });
 
--- index.test.js
+++ index.test.js
@@ -2,13 +2,13 @@ const request = require('supertest');
 const app = require('./index.js');
 
 test('DOM contains hello world', async () => {
-  const res = await request(app).get('/');
+  const res = await request(app).get('/test');
   expect(res.text).toEqual('Hello World!');
 });
 
-const server = app.listen(3000, function() {
-  console.log('running on port 3000!');
-});
+// const server = app.listen(3000, function() {
+//   console.log('running on port 3000!');
+// });
 
 afterAll(done => {
   server.close();
